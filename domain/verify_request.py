087e81b4d8e7494074cf7da455c67597bda5d2d0
artem <artemqq5@gmail.com>
2025-01-21 16:59:56 +0200

    update

--- domain/verify_request.py
+++ domain/verify_request.py
@@ -19,35 +19,18 @@ class VerifyRequest:
         return result
 
     def verify_signature(self, data: dict, signature: str, public_key: str) -> bool:
-        # Сортуємо дані перед серіалізацією
         sorted_message = orjson.dumps(self.__order_dict(data))
-        logger.info(f"Сортування та серіалізація даних для підпису: {sorted_message.decode()}")
-
-        # Створення хешу повідомлення
         digest = SHA256.new(sorted_message)
-
-        # Логування хешу
-        logger.info(f"Хеш (SHA256) для перевірки підпису: {digest.hexdigest()}")
-
-        # Завантажуємо публічний ключ
         public_key = RSA.importKey(public_key)
-
-        # Логування публічного ключа
-        logger.info(f"Публічний ключ для перевірки підпису: {public_key.export_key().decode()}")
-
-        # Декодуємо підпис з base64
         try:
             sig = base64.b64decode(signature)
-            logger.info(f"Декодований підпис (base64): {sig.hex()}")
         except Exception as e:
             logger.error(f"Помилка при декодуванні підпису: {e}")
             return False
 
-        # Перевірка підпису
         verifier = PKCS1_v1_5.new(public_key)
         is_verified = verifier.verify(digest, sig)
 
-        # Логування результату перевірки
         logger.info(f"Результат перевірки підпису: {'вірний' if is_verified else 'не вірний'}")
 
         return is_verified
--- main.py
+++ main.py
@@ -1,28 +1,32 @@
+import logging
 from fastapi import FastAPI, HTTPException, Header, Depends
 from fastapi.logger import logger
-
 from domain.verify_request import VerifyRequest
 from private_config import PUBLIC_KEY
 from fastapi import Request
 
+# Створюємо екземпляр FastAPI
 app = FastAPI()
 verify = VerifyRequest()
 
-import logging
-logging.basicConfig(level=logging.INFO)
+# Налаштовуємо формат логів
+logging.basicConfig(
+    level=logging.INFO,
+    format="%(asctime)s - %(name)s - %(levelname)s - %(message)s",  # Формат логів
+    handlers=[
+        logging.StreamHandler()  # Виводить логи в консоль
+    ]
+)
 
 
+# Функція для логування запиту
 async def log_request(request: Request):
-    # Логування інформації про запит
-    logger.info(f"Запит: {request.method} {request.url}")
-    logger.info(f"Хедери: {dict(request.headers)}")
+    logger.info(f"Header: {dict(request.headers)}")
 
-    # Параметри запиту
     query_params = dict(request.query_params)
     if query_params:
         logger.info(f"Параметри запиту: {query_params}")
 
-    # Тіло запиту
     try:
         body = await request.json()
         logger.info(f"Тіло запиту: {body}")
@@ -30,16 +34,19 @@ async def log_request(request: Request):
         logger.error(f"Не вдалося зчитати тіло запиту: {e}")
 
 
+# Функція для валідації запиту
 async def validate_request(request: Request, signature: str = Header(...)):
     await log_request(request)
     data_dict = await request.json()
-    logger.info(signature)
+    logger.info(f"Отриманий підпис: {signature}")
+
     is_verified = verify.verify_signature(data_dict, signature, PUBLIC_KEY)
 
     if not is_verified:
         raise HTTPException(status_code=400, detail="Невірний підпис")
 
 
+# Основний маршрут
 @app.post("/mtgooglebot/refound")
 async def refound(request: Request, validation: dict = Depends(validate_request)):
     logger.info("Запит успішний")
